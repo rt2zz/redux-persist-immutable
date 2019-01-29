49ad865d08ccf7aa3d8d2c3e8680209ed689787f
Alex Shtromberg <alex@helloheart.com>
2019-01-29 09:49:59 +0200

    Bugfix in autoMergeLevel2.

--- src/stateReconciler/autoMergeLevel2.js
+++ src/stateReconciler/autoMergeLevel2.js
@@ -2,7 +2,7 @@ import { fromJSGreedy } from "../fromJSGreedy";
 
 const merge = (state, payload) => {
   const incomeState = fromJSGreedy(payload);
-  return state.mergeDeep(incomeState);
+  return state.mergeDeepWith((oldVal, newVal) => newVal || oldVal, incomeState);
 }
 
 export default merge;
